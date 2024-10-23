9dcaa2446fd3a0244a2a9084e787a1eddd753f2b
Ragula-Saketh <ragulasaketh04@gmail.com>
2024-10-23 12:16:21 +0530

    Removed

--- src/main/java/com/example/javamavenjunithelloworld/saketh.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.example.javamavenjunithelloworld;
-
-public class saketh {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
-
-
