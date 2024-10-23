7b8398a3dc0b16e1b051559052b8ad256a938df4
Ragula-Saketh <ragulasaketh04@gmail.com>
2024-10-23 12:31:06 +0530

    SAKETHR0001 - Wrote a code to check if given year is leap year or not

---
+++ src/main/java/com/example/javamavenjunithelloworld/utility.java
@@ -0,0 +1,31 @@
+package main.java.com.example.javamavenjunithelloworld;
+
+import java.util.Scanner;
+
+public class utility {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        boolean isLeapYear = saketh(year);
+
+        if (isLeapYear) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+
+    public static boolean saketh(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
