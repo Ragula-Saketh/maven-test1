a76fa1c4994d91d38829b3ed78d23e51173b65ba
Ragula-Saketh <ragulasaketh04@gmail.com>
2024-10-23 12:02:18 +0530

    SAKETHR0001 - Created Hello World Program

---
+++ src/main/java/com/example/javamavenjunithelloworld/saketh.java
@@ -0,0 +1,9 @@
+package main.java.com.example.javamavenjunithelloworld;
+
+public class saketh {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+
